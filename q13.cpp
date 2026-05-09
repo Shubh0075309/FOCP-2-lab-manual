85765d0b4b824791541accc7fb6b3087c18e0663
Shubh Choudhary <shubh25csu228@ncuindia.edu>
2026-05-09 14:42:22 +0530

    q13.cpp

---
+++ q13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float a, b, c, d, r1, r2;
+    cin >> a >> b >> c;
+
+    d = b*b - 4*a*c;
+
+    if(d >= 0) {
+        r1 = (-b + sqrt(d)) / (2*a);
+        r2 = (-b - sqrt(d)) / (2*a);
+
+        cout << "Roots = " << r1 << " " << r2;
+    }
+    else
+        cout << "Imaginary roots";
+
+    return 0;
+}
