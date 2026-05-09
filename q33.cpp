1e0a46fa214d18c176cfd2e8bddd1464d1df03db
Shubh Choudhary <shubh25csu228@ncuindia.edu>
2026-05-09 14:56:46 +0530

    Create q33.cpp

---
+++ q33.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5], largest, second;
+
+    for(int i=0; i<5; i++)
+        cin >> arr[i];
+
+    largest = second = -9999;
+
+    for(int i=0; i<5; i++) {
+        if(arr[i] > largest) {
+            second = largest;
+            largest = arr[i];
+        }
+        else if(arr[i] > second && arr[i] != largest)
+            second = arr[i];
+    }
+
+    cout << "Largest = " << largest << endl;
+    cout << "Second Largest = " << second;
+
+    return 0;
+}
