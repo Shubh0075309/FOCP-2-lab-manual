41b3e55ca4512eaf8fd1271be9f21705bc61cb97
Shubh Choudhary <shubh25csu228@ncuindia.edu>
2026-05-09 14:45:17 +0530

    Create q17.cpp

---
+++ q17.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n, sum = 0, rem, temp;
+    cin >> n;
+
+    temp = n;
+
+    while(temp > 0) {
+        rem = temp % 10;
+        sum += rem * rem * rem;
+        temp /= 10;
+    }
+
+    if(sum == n)
+        cout << "Armstrong Number";
+    else
+        cout << "Not Armstrong";
+
+    return 0;
+}
