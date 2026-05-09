3e74e6ec71796b302d8ba538151ceefb5fee2df2
Shubh Choudhary <shubh25csu228@ncuindia.edu>
2026-05-09 14:43:23 +0530

    q15.cpp

---
+++ q15.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, num, max;
+    cin >> n;
+
+    cin >> max;
+
+    for(int i=2; i<=n; i++) {
+        cin >> num;
+        if(num > max)
+            max = num;
+    }
+
+    cout << "Largest = " << max;
+
+    return 0;
+}
